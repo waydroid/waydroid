08ef15b7fbdb05e53db06d67ac00e4b88c59af8c
luka177 <lukapanio@gmail.com>
2021-04-30 18:37:55 +0300

    Add some services to stop/start/restart

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -3,6 +3,17 @@ cd /home/anbox
 
 # just in case, stop Anbox 7
 stop anbox-container || true
+
+# restart cgroupfs-mount, else container may fail to start on some devices
+restart cgroupfs-mount
+
+# stop sensorfw
+# NOTE: it is temporary solution, that workes only on halium devices.
+stop sensorfw
+
+# start lxc-net, that sets up lxc bridge
+start lxc-net
+
 # umount rootfs if it was mounted
 umount -l rootfs || true
 ./mount.sh
