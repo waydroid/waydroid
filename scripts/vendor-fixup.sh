b5e46b24c821d87b67de3b63e627a83a85adb05f
Erfan Abdi <erfangplus@gmail.com>
2021-07-18 17:41:13 +0430

    anbox: Provide powervr GPU dependencies

--- scripts/vendor-fixup.sh
+++ scripts/vendor-fixup.sh
@@ -22,6 +22,12 @@ if [ -f /vendor/lib64/libladder.so ] && [ ! -f /home/anbox/tmp_vendor/lib64/libl
     wget https://github.com/GS290-dev/gigaset_gs290_dump/raw/full_k63v2_64_bsp-user-10-QP1A.190711.020-1597810494-release-keys/vendor/lib64/libladder.so
     cd ../../..
 fi
+if [ -f /vendor/lib64/libmpvr.so ] && [ ! -f /home/anbox/tmp_vendor/lib64/libmpvr.so ]; then
+    cp /vendor/lib64/libmpvr.so /home/anbox/tmp_vendor/lib64/
+fi
+if [ -f /vendor/lib/libmpvr.so ] && [ ! -f /home/anbox/tmp_vendor/lib/libmpvr.so ]; then
+    cp /vendor/lib/libmpvr.so /home/anbox/tmp_vendor/lib/
+fi
 sed -i "s/-service/-service --desktop_file_hint=unity8.desktop/" /home/anbox/tmp_vendor/etc/init/android.hardware.graphics.composer@2.1-service.rc
 umount /home/anbox/tmp_vendor
 rm -rf /home/anbox/tmp_vendor
