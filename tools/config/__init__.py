4488205505ae4895f60306b59831c843b18bf8c6
Erfan Abdi <erfangplus@gmail.com>
2022-02-04 20:10:07 +0330

    config: Switch to https for OTA channels
    * Fix issue #297

--- tools/config/__init__.py
+++ tools/config/__init__.py
@@ -71,8 +71,8 @@ if session_defaults["pulse_runtime_path"] == "None":
 
 channels_defaults = {
     "config_path": "/usr/share/waydroid-extra/channels.cfg",
-    "system_channel": "http://ota.waydro.id/system",
-    "vendor_channel": "http://ota.waydro.id/vendor",
+    "system_channel": "https://ota.waydro.id/system",
+    "vendor_channel": "https://ota.waydro.id/vendor",
     "rom_type": "lineage",
     "system_type": "VANILLA"
 }
