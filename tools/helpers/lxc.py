d2c283d7fcd6a1f778d362e7cda73c39988e0d0f
Alfred Neumayer <dev.beidl@gmail.com>
2022-05-07 14:18:25 +0200

    lxc: Optionally bind-mount NFC config
    Devices with libnfc-nci might have their NFC HAL crash when
    the config doesn't exist or contains invalid data. Fix it.

--- tools/helpers/lxc.py
+++ tools/helpers/lxc.py
@@ -121,6 +121,9 @@ def generate_nodes_lxc_config(args):
     for n in glob.glob("/tmp/run-*"):
         make_entry(n, options="rbind,create=dir,optional 0 0")
 
+    # NFC config
+    make_entry("/system/etc/libnfc-nci.conf", options="bind,optional 0 0")
+
     return nodes
 
 
