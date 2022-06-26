36f1098bc206b24efc1ef6cb11d697d0b5a3e839
Alessandro Astone <ales.astone@gmail.com>
2022-06-26 17:15:22 +0200

    Downgrade x86_64 to x86 if no SSE4.2

--- tools/helpers/arch.py
+++ tools/helpers/arch.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Oliver Smith
 # SPDX-License-Identifier: GPL-3.0-or-later
 import platform
+import logging
 
 def host():
     machine = platform.machine()
@@ -13,6 +14,15 @@ def host():
         "armv8l": "arm"
     }
     if machine in mapping:
-        return mapping[machine]
+        return maybe_remap(mapping[machine])
     raise ValueError("platform.machine '" + machine + "'"
                      " architecture is not supported")
+
+def maybe_remap(target):
+    if target == "x86_64":
+        with open("/proc/cpuinfo") as f:
+            if "sse4_2" not in f.read():
+                logging.info("x86_64 CPU does not support SSE4.2, falling back to x86...")
+                return "x86"
+
+    return target
