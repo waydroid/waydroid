43ab2b48853796f20715b1c883c3b4a7e6e95de2
Alessandro Astone <ales.astone@gmail.com>
2022-08-18 15:10:44 +0200

    Add mainline vulkan support
    Attempts to set the correct ro.hardware.vulkan value based on the
    loaded drm driver name

--- tools/helpers/gpu.py
+++ tools/helpers/gpu.py
@@ -4,10 +4,26 @@ import tools.helpers.props
 
 unsupported = ["nvidia"]
 
+def getKernelDriver(args, dev):
+    return tools.helpers.props.file_get(args, "/sys/class/drm/{}/device/uevent".format(dev), "DRIVER")
+
 def getDriNode(args):
     for node in glob.glob("/dev/dri/renderD*"):
         dev = os.path.basename(node)
-        driver = tools.helpers.props.file_get(args, "/sys/class/drm/{}/device/uevent".format(dev), "DRIVER")
-        if driver not in unsupported:
+        if getKernelDriver(args, dev) not in unsupported:
             return node
+    return ""
+
+def getVulkanDriver(args, dev):
+    mapping = {
+        "i915": "intel",
+        "amdgpu": "radeon",
+        "radeon": "radeon",
+        "panfrost": "panfrost",
+        "msm": "freedreno",
+        "vc4": "broadcom",
+    }
+    kernel_driver = getKernelDriver(args, dev)
+    if kernel_driver in mapping:
+        return mapping[kernel_driver]
     return ""
