e074cf4f8d4a1dabd6767306fbf51d25de90ebc4
Alessandro Astone <ales.astone@gmail.com>
2022-11-01 01:03:51 +0100

    Enable OpenGL ES 3.1

--- tools/helpers/lxc.py
+++ tools/helpers/lxc.py
@@ -250,7 +250,7 @@ def make_base_props(args):
 
     opengles = tools.helpers.props.host_get(args, "ro.opengles.version")
     if opengles == "":
-        opengles = "196608"
+        opengles = "196609"
     props.append("ro.opengles.version=" + opengles)
 
     if args.images_path not in tools.config.defaults["preinstalled_images_paths"]:
