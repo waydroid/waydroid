c0f595e3e7fd72c3d9352805481ff4d7bf526206
Alessandro Astone <ales.astone@gmail.com>
2023-01-20 01:16:41 +0100

    version: Don't fail when uname contains only two numbers

--- tools/helpers/version.py
+++ tools/helpers/version.py
@@ -4,4 +4,4 @@ def versiontuple(v):
     return tuple(map(int, (v.split("."))))
 
 def kernel_version():
-    return tuple(map(int, os.uname().release.split(".", 2)[:2]))
+    return tuple(map(int, re.match(r"(\d+)\.(\d+)", os.uname().release).groups()))
