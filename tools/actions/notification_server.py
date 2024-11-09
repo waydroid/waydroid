20a65957207ae29f36eac95d82b593e5b0fd6021
Marius Melzer <marius@rasumi.net>
2024-11-09 15:57:52 +0100

    Pass actual android notifications to Linux
    notifications_server:
    - parse more details about the notifications from the dump, e.g.
      message hash, title + text (instead of just ticker) and interesting
      flags
    - differentiate between new notification and notification updates
    - send removal messages when android notifications disappear (e.g. when
      messenger messages are read in the android app)
    
    notification_client:
    - add notification update and notification removal
    - only use urgency 1 when android notification would have triggered the
      notification led ("show_light"), otherwise 0
    - ignore android group banner and background
    - save notification action handlers and change the way invoked action
      works (added by FakeShell)
    
    Includes fixes + improvements by FakeShell.

--- tools/actions/notification_server.py
+++ tools/actions/notification_server.py
@@ -2,14 +2,12 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import re
-import sys
 import time
 import logging
 import threading
 import subprocess
 import dbus
 import dbus.service
-from collections import defaultdict
 
 running = False
 loop_thread = None
@@ -18,42 +16,56 @@ class INotification(dbus.service.Object):
     def __init__(self, bus_name, object_path='/id/waydro/Notification'):
         dbus.service.Object.__init__(self, bus_name, object_path)
 
-    @dbus.service.signal(dbus_interface='id.waydro.Notification', signature='si')
-    def NewMessage(self, package_name, count):
+    @dbus.service.signal(dbus_interface='id.waydro.Notification', signature='ssssssbbbt')
+    def NewMessage(self, msg_hash, msg_id, package_name, ticker, title, text, is_foreground_service,
+                   is_group_summary, show_light, when):
         pass
 
-def get_notifications(old_notification):
-    global running
-    notification_count = defaultdict(int)
+    @dbus.service.signal(dbus_interface='id.waydro.Notification', signature='sssssssbbbt')
+    def UpdateMessage(self, msg_hash, replaces_hash, msg_id, package_name, ticker, title, text,
+                      is_foreground_service, is_group_summary, show_light, when):
+        pass
+
+    @dbus.service.signal(dbus_interface='id.waydro.Notification', signature='s')
+    def DeleteMessage(self, msg_hash):
+        pass
+
+def get_notifications(_old_notification):
+    notifications = {}
+    old_notifications = {}
 
     system_bus = dbus.SystemBus()
     bus_name = dbus.service.BusName('id.waydro.Notification', system_bus)
     interface = INotification(bus_name, object_path='/id/waydro/Notification')
 
     notification_command = [
-        "lxc-attach", "-P", "/var/lib/waydroid/lxc",
-        "-n", "waydroid", "--clear-env", "--", "/system/bin/sh", "-c", "dumpsys notification"
+        "lxc-attach", "-P", "/var/lib/waydroid/lxc", "-n", "waydroid", "--clear-env", "--",
+        "/system/bin/sh", "-c", "dumpsys notification --noredact"
     ]
 
     applist_command = [
-        "lxc-attach", "-P", "/var/lib/waydroid/lxc",
-        "-n", "waydroid", "--clear-env", "--", "/system/bin/sh", "-c", "pm list packages -3"
+        "lxc-attach", "-P", "/var/lib/waydroid/lxc", "-n", "waydroid", "--clear-env", "--",
+        "/system/bin/sh", "-c", "pm list packages -3"
     ]
 
     logging.info("Starting notification server service")
 
     while running:
-        notification_process = subprocess.Popen(notification_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        notification_process = subprocess.Popen(notification_command, stdout=subprocess.PIPE,
+                                                stderr=subprocess.PIPE)
         notification_stdout, notification_stderr = notification_process.communicate()
 
         if notification_stderr:
+            for line in notification_stderr.splitlines():
+                logging.error(line)
+
             time.sleep(3)
             continue
 
         notification_stdout = notification_stdout.decode()
-        notification_filtered_output = re.findall(r"NotificationRecord.*", notification_stdout)
 
-        applist_process = subprocess.Popen(applist_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        applist_process = subprocess.Popen(applist_command, stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE)
         applist_stdout, applist_stderr = applist_process.communicate()
 
         if applist_stderr:
@@ -63,26 +75,121 @@ def get_notifications(old_notification):
         applist_stdout = applist_stdout.decode()
         packages = [line.split(':')[1] for line in applist_stdout.splitlines() if ':' in line]
 
-        for line in notification_filtered_output:
-            fields = line.split("|")
+        # parse notifications
+        current_msg_hash = None
+        multiline_ticker = None
+        multiline_text = None
+        for line in notification_stdout.splitlines():
+            # ticker and text may be multi line and there seems no better way
+            # to parse this with the dumpsys format
+            if multiline_ticker:
+                if line.startswith("  "):
+                    notifications[current_msg_hash]['ticker'] = multiline_ticker
+                    multiline_ticker = None
+                else:
+                    multiline_ticker = multiline_ticker + "\n" + line
+                    continue
+            elif multiline_text:
+                if line.startswith("  "):
+                    notifications[current_msg_hash]['text'] = multiline_text[:-1]
+                    multiline_text = None
+                else:
+                    multiline_text = multiline_text + "\n" + line
+                    continue
 
-            if len(fields) > 1:
-                notification_name = fields[1].strip()
-                if notification_name in packages:
-                    notification_count[notification_name] += 1
+            if "NotificationRecord" in line:
+                current_msg_hash = None
+                fields = line.split("|")
+                if len(fields) > 3:
+                    package_name = fields[1].strip()
+                    res = re.search(r'NotificationRecord\(([^:]+):', line)
+                    if package_name in packages and res:
+                        current_msg_hash = res.group(1)
+                        notifications[current_msg_hash] = {
+                            'package_name': package_name,
+                            'msg_id': fields[2],
+                            'ticker': '',
+                            'title': '',
+                            'text': '',
+                            'is_foreground_msg': False,
+                            'is_group_summary': False,
+                            'show_light': False,
+                            'when': 0
+                        }
+            elif current_msg_hash:
+                msg_hash = current_msg_hash
 
-        for package_name, count in notification_count.items():
-            old_count = old_notification.get(package_name, 0)
+                if "  tickerText=" in line:
+                    multiline_ticker = line.replace('tickerText=','').strip()
+                elif "  android.title=" in line:
+                    res = re.search(r'android.title=\w+\s*\((.*)\)$', line)
+                    if res:
+                        notifications[msg_hash]['title'] = res.group(1).strip()
+                elif "  android.text=" in line:
+                    res = re.search(r'android.text=\w+\s*\((.*)$', line)
+                    if res:
+                        multiline_text = res.group(1).strip()
+                elif "  flags=" in line:
+                    flags = int(line.replace('flags=','').strip(), 0)
+                    notifications[msg_hash]['is_foreground_msg'] = \
+                      (flags & 0x00000040) != 0
+                    notifications[msg_hash]['is_group_summary'] = \
+                      (flags & 0x00000200) != 0
+                elif "  mLight=" in line:
+                    notifications[msg_hash]['show_light'] = \
+                      line.replace('mLight=','').strip() != "null"
+                elif "  when=" in line:
+                    notifications[msg_hash]['when'] = \
+                      int(line.replace('when=','').strip(), 0)
 
-            if count > old_count:
-                # logging.info(f"You have {count} notifications from {package_name}")
-                interface.NewMessage(package_name, count)
+        # analyse and send notifications
+        updated_hashes = set()
+        for msg_hash, n in notifications.items():
+            # this happens e.g. for foreground applications when they start.
+            # currently they are ignored, but they could also be transformed
+            # into a "<app> started in background" message
+            if (n['ticker'] == 'null' or n['ticker'] == '') and (n['title'] == '' or n['text'] == ''):
+                logging.error("Ticker is null and title or text are empty. skipping")
+                continue
 
-        old_notification = notification_count.copy()
-        notification_count = defaultdict(int)
+            # check if notification is new or an update (= message hash did not exist before)
+            if msg_hash not in old_notifications:
+                # search for msg_id in old notifications to see if this is an update
+                is_update_of = [h for h, o in old_notifications.items()
+                                if o['msg_id'] == n['msg_id']]
+                if is_update_of:
+                    #logging.info("Update Message (%s):", msg_hash)
+                    #logging.info(n)
+
+                    if len(is_update_of) > 1:
+                        logging.warning("Warning: Multiple messages w same msg_id at the same time")
+
+                    # send update
+                    updated_hashes.add(is_update_of[0])
+                    interface.UpdateMessage(msg_hash, is_update_of[0], n['msg_id'],
+                                            n['package_name'], n['ticker'], n['title'], n['text'],
+                                            n['is_foreground_msg'], n['is_group_summary'],
+                                            n['show_light'], n['when'])
+                else:
+                    #logging.info("New Message (%s):", msg_hash)
+                    #logging.info(n)
+
+                    # send new message
+                    interface.NewMessage(msg_hash, n['msg_id'], n['package_name'], n['ticker'],
+                                         n['title'], n['text'], n['is_foreground_msg'],
+                                         n['is_group_summary'], n['show_light'], n['when'])
+
+        # send removal messages
+        for msg_hash in old_notifications:
+            if msg_hash not in notifications and msg_hash not in updated_hashes:
+                #logging.info(f"Send removal message: {msg_hash}")
+                interface.DeleteMessage(msg_hash)
+
+        old_notifications = notifications
+        notifications = {}
         time.sleep(3)
 
-def start(args):
+def start(_args):
     global running
     global loop_thread
 
@@ -90,9 +197,8 @@ def start(args):
     loop_thread = threading.Thread(target=get_notifications, args=({},))
     loop_thread.start()
 
-def stop(args):
+def stop(_args):
     global running
-    global loop_thread
 
     running = False
     if loop_thread is not None:
