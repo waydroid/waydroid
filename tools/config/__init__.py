dced8069c8afdf3c579a570980465ce7b0a44cb1
Alessandro Astone <ales.astone@gmail.com>
2025-06-24 11:19:24 +0200

    Upver to 1.5.4

--- tools/config/__init__.py
+++ tools/config/__init__.py
@@ -12,7 +12,7 @@ from tools.config.save import save
 #
 # Exported variables (internal configuration)
 #
-version = "1.5.3"
+version = "1.5.4"
 tools_src = os.path.normpath(os.path.realpath(__file__) + "/../../..")
 
 # Keys saved in the config file (mostly what we ask in 'waydroid init')
