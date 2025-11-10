a62dfb618548ced9aa43da1c8f9d8aa39c1f92ce
Alessandro Astone <ales.astone@gmail.com>
2025-11-11 00:28:08 +0100

    logging: Fixup starting when logfile does not exist

--- tools/helpers/logging.py
+++ tools/helpers/logging.py
@@ -71,7 +71,10 @@ def init(args):
 
     # Add file log handler
     if args.action == "container" and not args.details_to_stdout:
-        os.chmod(args.log, 0o644)
+        try:
+            os.chmod(args.log, 0o644)
+        except:
+            pass
         handler = RotatingFileHandler(args.log, maxBytes=5*1024*1024, backupCount=1)
         handler.setFormatter(logging.Formatter("(%(process)d) [%(asctime)s] %(message)s",
                                       datefmt="%a, %d %b %Y %H:%M:%S"))
