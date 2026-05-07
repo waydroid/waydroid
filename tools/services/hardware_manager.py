1a3a7ac008e88c16cd5c81a070d2e126d510c9ee
SupeChicken666 <me@supechicken666.dev>
2026-05-07 16:36:45 +0800

    hardware_manager: Handle Android reboot/shutdown properly
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- tools/services/hardware_manager.py
+++ tools/services/hardware_manager.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 import logging
 import threading
+import time
 import os
 import tools.actions.container_manager
 import tools.actions.session_manager
@@ -39,10 +40,32 @@ def start(args):
         helpers.protocol.set_aidl_version(args)
         helpers.lxc.start(args)
 
+    def shutdownRequest(reason, in_thread = False):
+        if not in_thread:
+            threading.Thread(target=shutdownRequest, args=(reason, True)).start()
+            return
+
+        is_reboot = reason and reason.startswith("1")
+        tries = 0
+
+        while helpers.lxc.status(args) != "STOPPED":
+            if tries >= 30:
+                logging.debug(f"Android is still not stopped, give up waiting after {tries} seconds")
+                return
+
+            logging.debug("Waiting for Android to shutdown")
+            time.sleep(1)
+            tries += 1
+
+        if is_reboot:
+            helpers.lxc.start(args)
+        else:
+            tools.actions.session_manager.stop(args)
+
     def service_thread():
         while not stopping:
             IHardware.add_service(
-                args, enableNFC, enableBluetooth, suspend, reboot, upgrade)
+                args, enableNFC, enableBluetooth, suspend, reboot, upgrade, shutdownRequest)
 
     global stopping
     stopping = False
